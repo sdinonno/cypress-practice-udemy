6de26d64ed10a2f1908a7a576c04b2deb449d022
Sofia Di Nonno <sofidinonno@gmail.com>
2023-07-21 16:23:05 -0300

    First commit

---
+++ cypress/support/commands.js
@@ -0,0 +1,43 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('search', (value)=>{
+    cy.fixture('index').then((index)=>{
+        cy.get(index.searchBox).type(value);
+        cy.get(index.searchButton).click();
+    })
+})
+
+Cypress.Commands.add('login', (email, password)=>{
+    cy.fixture('login').then((login)=>{
+        cy.get(login.loginLink).click();
+        cy.get(login.email).type(email);
+        cy.get(login.password).type(password);
+        cy.get(login.loginButton).click();
+    })
+})
+
+
