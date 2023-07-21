4e7b7019f9b62914776f0c79a389ed70d0946cb9
Sofia Di Nonno <sofidinonno@gmail.com>
2023-07-21 17:53:03 -0300

    New test for invalid login with incorrect password

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,11 +2,19 @@ describe('Login', ()=>{
     beforeEach(()=>{
         cy.visit('/')
     })
+
     it('Login with incorrect email', ()=>{
         cy.login('something', 'other')
         cy.fixture('login').then((login)=>{
             cy.get(login.incorrectLoginBanner).should('contain','Invalid email address');
         })
         
+    })
+
+    it('Login with incorrect password', ()=>{
+        cy.login('sofiaditest1@gmail.com', "abdc123")
+        cy.fixture('login').then((login)=>{
+            cy.get(login.incorrectLoginBanner).should('contain','Authentication failed.');
+        })
     })
 })
