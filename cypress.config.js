850ad8187a218d069f14720cfe3f781c3051c8e7
Sofia Di Nonno <sofidinonno@gmail.com>
2023-07-27 13:47:46 -0300

    New configs for Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://www.automationpractice.pl',
+    supportFile: 'cypress/support/commands.js'
   },
 })
